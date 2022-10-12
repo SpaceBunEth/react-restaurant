24f48def9129968166cfc677d4e850ac64c2f83e
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-10-12 20:17:43 +0000

    added components

--- app/src/App.js
+++ app/src/App.js
@@ -6,22 +6,57 @@ const baseURL = "https://astute-baton-362318.ue.r.appspot.com/api/json/";
 
 export default function App() {
   const [post, setPost] = React.useState(null);
-
-  console.log({post, setPost})
+  
+  console.log({post})
 
   React.useEffect(() => {
     axios.get(baseURL).then((response) => {
-        console.log(response.data)
+        console.log(response)
       setPost(response.data);
     });
   }, []);
 
-  if (!post) return null;
 
+  if (!post) return null;
+  console.log('AFTER: ',post)
+  console.log(post[0])
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-    </div>
+    <>
+    <Info />
+    </>
+    
+    // <div>
+    //   <h1>Good Morning</h1>
+    //   <p>{post[0].title}</p>
+    //   <p>{post[0].price}</p>
+    //   {/* <p>{post[0].category.title}</p> */}
+    //   <p>{post[0].description}</p>
+    // </div>
   );
+}
+
+export function Info(){
+  const restaurantInfo = {
+    hours:[
+    'Monday 8:00am - 5:00pm',
+    'Tuesday 8:00am - 5:00pm',
+    'Wednesday 8:00am - 5:00pm',
+    'Thursday 8:00am - 5:00pm',
+    'Friday 8:00am - 9:00pm',
+    'Saturday 10:00am - 11:00pm',
+    'Sunday Closed'
+  ],
+    
+    phone: '(859)-555-5555',
+
+    address:{
+      street: '405 East Main Str',
+      city: 'Lexingtion',
+      state: 'KY',
+      zip: '40502'
+    }
+
+  }
+  const [info, setInfo] = React.useState(restaurantInfo);
+  console.log(info)
 }
