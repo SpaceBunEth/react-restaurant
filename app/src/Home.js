5d4dcd5590933f6601b47e22f14fb3e8a11c2ac0
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-12-24 00:22:57 +0000

    cleaning up comments

--- app/src/Home.js
+++ app/src/Home.js
@@ -43,23 +43,3 @@ export default function Home(props){
 
 
 
-// export default function Home(){
-//     const reactImg = {
-//         name:'SpaceBun',
-//         imageUrl: ReactImgs,
-//         classImg: 'card-img'
-//     }
-
-//     return(
-//     <>
-//         <div className="card text-bg-dark">
-//             <img src={reactImg.imageUrl} className={reactImg.classImg} alt={reactImg.name} />
-//             <div className="card-img-overlay">
-//                 {/* <h5 className="card-title">Card title</h5>
-//                 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-//                 <p className="card-text"><small>Last updated 3 mins ago</small></p> */}
-//             </div>
-//         </div>
-//     </>
-//     );
-// }
